2cf4a9688e96574ce0d35fe5553a59271d62d580
Artem Bondarenko <Regall.B@yandex.ru>
2024-01-15 22:57:15 +0300

    Another attemp to fix imports

--- service/main.py
+++ service/main.py
@@ -7,11 +7,11 @@ import psycopg2
 import boto3
 from botocore.exceptions import NoCredentialsError, ClientError
 
-from .utilities.get_data import (get_keys, get_tickers, get_history_by_ticker)
-from .utilities.get_models import (get_models_by_ticker_timeframe, get_models_by_ticker, get_model)
-from .utilities.generate_features import (gen_re_rsi, gen_re_rsi_mtf, gen_rsi, gen_ema, generate_features, cleanup_and_prepare_data)
-from .utilities.train_model import (train_model)
-from .utilities.predict_validate import (get_error_metrics, predict, validate_model)
+from service.utilities.get_data import (get_keys, get_tickers, get_history_by_ticker)
+from service.utilities.get_models import (get_models_by_ticker_timeframe, get_models_by_ticker, get_model)
+from service.utilities.generate_features import (gen_re_rsi, gen_re_rsi_mtf, gen_rsi, gen_ema, generate_features, cleanup_and_prepare_data)
+from service.utilities.train_model import (train_model)
+from service.utilities.predict_validate import (get_error_metrics, predict, validate_model)
 
 #Inititalize app
 ml_app = FastAPI()
@@ -112,80 +112,4 @@ async def predict(model_name : str, ticker : str, timeframe : Timeframe, num_bar
 
     return {"y_hat" : y_hat, "validation_matrics" : metrics}
     
-    
-    
-    
-    
-    
-
-    
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                            
-                            
-@app.get('/dog',
-        summary = "Get Dogs",
-        operation_id =  "get_dogs_dog_get", 
-        response_model = Dog)
-async def get_dog(kind : DogType | None = None) -> list[Dog]:
-    
-    response = []
-    
-    if kind == None:
-        response.append(dogs_db)
-        
-    else:
-        for dog in list(dogs_db.values()):
-        
-            if dog.kind == kind:
-                            
-                response.append(dog)
-             
-    return response
-    
-    
-@app.post('/dog',
-        summary = "Create Dog",
-        operation_id =  "create_dog_dog_post", 
-        response_model = Dog)
-async def post_dog(name : str, pk: int, kind : DogType) -> Dog:
-    
-    
-    new_dog_name = name
-    new_dog_pk = pk
-    new_dog_kind = DogType(kind)
-
-    new_dog = Dog(name = new_dog_name, pk = new_dog_pk, kind = new_dog_kind)
-    if new_dog.pk in dogs_db.keys():
-        
-        raise HTTPException(status_code = 422, detail = "Dog with a given 'pk' already exists")
-        
-    else: 
-        dogs_db[pk] = new_dog
-        # reponse = {
-        #     "name" : name,
-        #     "pk" : pk,
-        #     "kind" : kind                  
-        # }
-        response = new_dog
-        
-        return response
     
